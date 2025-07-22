3fa36e9787c9d61e0b6e2b309ac1f53933c209ef
Andy Wilson <atwilso@sandia.gov>
2025-07-22 17:58:34 -0600

    Disable libcpp availability check in build.sh
    There is a new mechanism in libc++ that checks to see when features
    became available (in the underlying OS distribution) compared to
    which version of clang is running.  This was firing on something
    (from_chars, I think, related to floats) in boost::uuid.
    
    The conda-forge knowledge repository has this to say:
    
    """
    However, since conda-forge ships its own (modern) libcxx we can ignore
    these checks because these symbols are in fact available. To do so, add
    _LIBCPP_DISABLE_AVAILABILITY to the defines. For example
    
    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY
    """
    
    I have added that flag to the C++ compiler arguments in build.sh.

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,7 @@ cmake ${CMAKE_ARGS} \
         -D CMAKE_INSTALL_PREFIX:PATH=${HERE}/install \
         -D BUILD_TESTING=OFF \
         -D BUILD_EXAMPLES=OFF \
+        -D CMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" \
         $SRC_DIR
 make -j${CPU_COUNT}
 make install
